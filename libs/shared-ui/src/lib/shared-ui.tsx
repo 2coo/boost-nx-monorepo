aa48a0a580fcfd5c75dd564d04edc546c9c864e3
Tuvshinbayar Tuvshinzul <tuvshinbayar@boost.mn>
2023-02-28 01:13:18 +0800

    initialize

---
+++ libs/shared-ui/src/lib/shared-ui.tsx
@@ -0,0 +1,12 @@
+import styles from './shared-ui.module.css';
+
+/* eslint-disable-next-line */
+export interface SharedUiProps {}
+
+export function PageHeader(props: SharedUiProps) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to SharedUi!</h1>
+    </div>
+  );
+}
